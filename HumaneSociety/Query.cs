f2544fb3ed2d235249e698e603d89a44512284d5
Matthew Nelson <mnelson251@gmail.com>
2019-01-27 13:07:36 -0600

    update animal shot working, will enter new record for new animal shot, will update date if animal has shot on record

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -265,21 +265,21 @@ namespace HumaneSociety
             HumaneSocietyDataContext db = new HumaneSocietyDataContext();
 
             var shotUpdateAnimal = db.AnimalShots.Where(s => s.AnimalId == animal.AnimalId).SingleOrDefault();
-            var shotUpdateShot = db.AnimalShots.Where(f => f.Shot.Name == word).ToArray();
-            var shotz = shotUpdateShot[0];
+            var shotUpdateShot = db.AnimalShots.Where(f => f.Shot.Name == word && f.AnimalId == animal.AnimalId).ToArray();
+            var shotz = shotUpdateShot;
             
-            if (shotUpdateAnimal != null)
+            if (shotUpdateShot.Length > 0)
             {
-                AnimalShot animalShot = new AnimalShot { DateReceived = DateTime.Now };
-                db.AnimalShots.InsertOnSubmit(animalShot);
+                var thisShot = shotz[0];
+                thisShot.DateReceived = DateTime.Now;
+                db.SubmitChanges();
             }
             
-            if (shotUpdateShot == null)
+            if (shotUpdateAnimal == null)
             {
-                AnimalShot animalShot = new AnimalShot { AnimalId = animal.AnimalId, ShotId = shotz.Shot.ShotId, DateReceived = DateTime.Now };
-                
-                    db.AnimalShots.InsertOnSubmit(animalShot);
-                    db.SubmitChanges();  
+                AnimalShot animalShot = new AnimalShot { AnimalId = animal.AnimalId, ShotId = 6, DateReceived = DateTime.Now };
+                db.AnimalShots.InsertOnSubmit(animalShot);
+                db.SubmitChanges();  
             }
             
 
