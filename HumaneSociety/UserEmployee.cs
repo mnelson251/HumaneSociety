231105bc2ee133735632173b9f353ae368642292
Matthew Nelson <mnelson251@gmail.com>
2019-01-28 14:25:12 -0600

    all comments removed from method

--- HumaneSociety/UserEmployee.cs
+++ HumaneSociety/UserEmployee.cs
@@ -507,7 +507,7 @@ namespace HumaneSociety
             Console.Clear();
 
             Console.WriteLine("Choose The Name Of The Plan You Wish To Modify");
-           // string planToMod = Console.ReadLine();
+           
             DietPlan ModThisPlan = Query.FindDietPlan(UserInterface.GetUserInput());
 
             Console.WriteLine("Choose a feild to modify: 1. Name  2. Food Type  3. Amount, in Cups");
@@ -515,25 +515,25 @@ namespace HumaneSociety
 
             HumaneSocietyDataContext db = new HumaneSocietyDataContext();
 
-            //THIS IS THE LINE I ADDED
+            
             var dietPlantFromDb = db.DietPlans.Where(d => d.Name == ModThisPlan.Name).Single();
 
             if (fieldToMod == 1)
             {
                 string newName = UserInterface.GetStringData("name", "the new");
-                //THIS IS WHERE I MADE CHANGES --SO THAT THE DIET PLAN THATS IN THE DB CHANGES
+                
                 dietPlantFromDb.Name = newName;
             }
             else if (fieldToMod == 2)
             {
                 string newFoodType = UserInterface.GetStringData("type", "the new");
-                //THIS IS WHERE I MADE CHANGES --SO THAT THE DIET PLAN THATS IN THE DB CHANGES
+               
                 dietPlantFromDb.FoodType = newFoodType;
             }
             else if (fieldToMod == 3)
             {
                 int newFoodAmount = UserInterface.GetIntegerData("amount", "the new food");
-                //THIS IS WHERE I MADE CHANGES --SO THAT THE DIET PLAN THATS IN THE DB CHANGES
+                
                 dietPlantFromDb.FoodAmountInCups = newFoodAmount;
             }
             else
