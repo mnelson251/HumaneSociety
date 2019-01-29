6ae9306d4acab26993765b28aa6742adb2a07801
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2019-01-29 15:22:55 -0600

    Fixed minor bugs

--- HumaneSociety/UserInterface.cs
+++ HumaneSociety/UserInterface.cs
@@ -175,10 +175,10 @@ namespace HumaneSociety
             while (isSearching)
             {
                 Console.Clear();
-                List<string> options = new List<string>() { "Select Search Criteia: (Enter number and choose finished when finished)", "1. Category", "2. Name", "3. Age", "4. Demeanor", "5. Kid friendly", "6. Pet friendly", "7. Weight", "8. ID", "9. Finished" };
+                List<string> options = new List<string>() { "Select Search Criteia: (Enter number and choose finished when finished)", "1. Category", "2. Name", "3. Age", "4. Gender", "5. Demeanor", "6. Kid friendly", "7. Pet friendly", "8. Weight", "9. ID", "10. Finished" };
                 DisplayUserOptions(options);
                 string input = GetUserInput();
-                if (input.ToLower() == "9" || input.ToLower() == "finished")
+                if (input.ToLower() == "10" || input.ToLower() == "finished")
                 {
                     isSearching = false;
                     continue;
